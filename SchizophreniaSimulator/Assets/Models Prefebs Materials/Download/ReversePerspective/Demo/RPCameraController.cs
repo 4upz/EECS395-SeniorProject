dd71db9ff65729e0f7d2c494fc2b801a97292f8e
Gareth Valentin <mxl900@case.edu>
2019-11-04 17:37:01 -0500

    fix dolly

--- SchizophreniaSimulator/Assets/Models Prefebs Materials/Download/ReversePerspective/Demo/RPCameraController.cs
+++ SchizophreniaSimulator/Assets/Models Prefebs Materials/Download/ReversePerspective/Demo/RPCameraController.cs
@@ -17,7 +17,9 @@ public class RPCameraController : MonoBehaviour
 	private SmoothValue m_smoothPerspective = new SmoothValue(0.0f, 10f);
 	private SmoothValue m_smoothSizeScale   = new SmoothValue(0.0f, 10f);
 
-	void Start() {
+    private float timer = 0.0f;
+
+    void Start() {
 		if (rPCamera == null) throw new UnityException ("Perspective Camera not set");
 		if (rPCamera.pivot == null) throw new UnityException ("Pivot not set");
 
@@ -30,69 +32,28 @@ public class RPCameraController : MonoBehaviour
 
 	void Update() {
 
-        /*
-		float mx = Input.mousePosition.x / Screen.width  * 2f - 1f; // -1..1
-		float my = Input.mousePosition.y / Screen.height * 2f - 1f;
-		m_smoothMouseX.target = mx;
-		m_smoothMouseY.target = my;
-		float mxs = m_smoothMouseX.GetValue();
-		float mys = m_smoothMouseY.GetValue();
 
         
-		// rotate
-		m_movingFlatDirection *= Quaternion.Euler(0f, Mathf.Pow(mxs, 3) * 2f, 0f);
-		Quaternion movingDirection = m_movingFlatDirection * Quaternion.Euler(0f, mxs * 60f, 0f);
+        timer += Time.deltaTime;
+        int seconds = (int)(timer % 60);
 
-		Quaternion cameraRotation = Quaternion.Euler(-mys * 90f, movingDirection.eulerAngles.y, 0);
+        
+        if (timer > 3 && timer < 5 && rPCamera.perspective > -5)
+        {
+            rPCamera.perspective -= 7 * Time.deltaTime;
+            rPCamera.UpdateProjection(true);
+        }
 
-		rPCamera.pivot.localRotation = cameraRotation;
-
-		// move
-		float mv = Input.GetAxis("Vertical");
-		float mh = Input.GetAxis("Horizontal");
-		Vector3 move = movingDirection * (Vector3.forward * mv + Vector3.right * mh) * m_camera.orthographicSize * 0.03f;
-
-		rPCamera.pivot.position += move;
-        */
-
-        float tt = Time.realtimeSinceStartup;
-        print(tt);
-
-        // mouse wheel - zoom and FOV
-        float w = Input.GetAxis("Mouse ScrollWheel");
-
-		if (w != 0) {
-			if (Input.GetKey(KeyCode.LeftAlt)) { // change orthographic Size (FOV)
-				m_smoothSizeScale.target += w;
-			} else { // change perspective
-				m_smoothPerspective.target += w / m_camera.orthographicSize;
-			}
-		}
-
-		bool prepareProjection = false;
-		bool updateProjection  = false;
-
-		if (m_smoothSizeScale.IsMoving())
-		{
-			float v = m_smoothSizeScale.GetValue();
-			float newSize = m_cameraInitialSize * Mathf.Exp(v * 2.0f);
-			m_smoothPerspective.target *= m_camera.orthographicSize / newSize;
-			m_camera.orthographicSize = newSize;
-			prepareProjection = true;
-		}
-		if (m_smoothPerspective.IsMoving())
-		{
-			//Debug.Log("smoothPerspective: " + m_smoothPerspective.value + " " + m_smoothPerspective.target);
-			float v = m_smoothPerspective.GetValue();
-			rPCamera.perspective = -v * 0.8f * 100f;
-			updateProjection = true;
-		}
-
-		// update camera projection
-		if (prepareProjection || updateProjection) {
-			rPCamera.UpdateProjection(prepareProjection);
-		}
-
+        float count = (timer - 5) / 3.18f;
+        float speed = (Mathf.Cos(count - 3.14f) + 1) / 2 + 1;
+        if (timer > 5 && rPCamera.perspective < 100) {
+            
+            rPCamera.perspective += 15 * speed * Time.deltaTime;
+            rPCamera.UpdateProjection(true);
+        }
+        Debug.Log(timer);
+        Debug.Log("speed" + speed);
+        Debug.Log(rPCamera.perspective);
     }
 
 }
