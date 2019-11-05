9910afda2780fb042b7d53fbd5034bbdc84efa93
Arik Stewart <ams382@case.edu>
2019-11-04 19:02:58 -0500

    Menu UI In Progress

---
+++ SchizophreniaSimulator/Assets/Scripts/MenuButtons.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuButtons : MonoBehaviour
+{
+   // Loads a given scene within the game
+   public void LoadScene(string sceneName){
+       SceneManager.LoadScene(sceneName);
+   }
+
+    // Exits to the main menu of the game
+    public void ExitToMainMenu(){
+        SceneManager.LoadScene("MainMenu");
+    }
+
+   // Exits the entire application
+   public void ExitGame(){
+       Application.Quit();
+   }
+}
