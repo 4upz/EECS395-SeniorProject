88d296900f37cded0160c2b52a0a4cc5e0fba50d
Arik Stewart <ams382@case.edu>
2019-12-01 13:32:27 -0500

    Working UI Introduction and Button Interaction

---
+++ SchizophreniaSimulator/Assets/Scripts/UIControls/IntroductionMessage.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroductionMessage : MonoBehaviour
+{
+
+    [SerializeField] private GameObject[] introText;        // Array of text to be shown for the simulation intro
+    [SerializeField] private GameObject playerTeleporting; // The player's ability to move
+
+    private Button continueButton;      // The button to navigate the introduction messages
+
+    private int activeTextIndex;    // Index of the active slide in introSlides
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        // Grab the button component of the continue button
+        continueButton = gameObject.GetComponentInChildren<Button>();
+        // Add a listener to the continue button that calls the given function when clicked
+        continueButton.onClick.AddListener(ToggleIntroText);
+
+        // Disable player movement during introduction
+        playerTeleporting.SetActive(false);
+
+        // Set the active index to the first slide
+        activeTextIndex = 0;
+    }
+
+    // Toggles through the slides of intro messages and disables them after the last one
+    public void ToggleIntroText(){
+        introText[activeTextIndex].SetActive(false);  //Disable the currently active slide
+        // If the current slide is the last slide, then disable the continue button
+        if (activeTextIndex == introText.Length-1){
+            continueButton.gameObject.SetActive(false);
+            playerTeleporting.SetActive(true);
+        }
+        // Else, set the next slide active
+        else {
+            introText[++activeTextIndex].SetActive(true);
+        }
+        Debug.Log(activeTextIndex);
+    }
+}
