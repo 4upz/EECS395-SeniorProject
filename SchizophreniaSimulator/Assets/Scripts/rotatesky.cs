76b706fc5aa54bd5223c10b18732a63c915486d0
Gareth Valentin <mxl900@case.edu>
2019-12-01 23:15:06 -0500

    fix

---
+++ SchizophreniaSimulator/Assets/Scripts/rotatesky.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotatesky : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * speed);
+    }
+}
