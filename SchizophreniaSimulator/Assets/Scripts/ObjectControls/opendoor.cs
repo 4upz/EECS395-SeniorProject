3829b4091ce6487a8556839c862b030543a90a61
Gareth Valentin <mxl900@case.edu>
2019-12-01 21:36:00 -0500

    fix

---
+++ SchizophreniaSimulator/Assets/Scripts/ObjectControls/opendoor.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class opendoor : MonoBehaviour
+{
+    private bool rotate = false;
+    public int whichdoor;
+    private GameObject playerObj;
+    private float timer = 0.0f;
+
+    void Start()
+    {
+        playerObj = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+
+        float rotation = 30 * Time.deltaTime;
+        float loc = playerObj.transform.position.z;
+        // Debug.Log(loc);
+        if (loc < 4)
+        {
+
+            timer += Time.deltaTime;
+            print(timer);
+            if (timer < 4)
+            {
+                rotate = true;
+            }
+            else {
+                rotate = false;
+            }
+        }
+
+        if (rotate) {
+            if (whichdoor == 0)
+            {
+                transform.RotateAround(new Vector3(-2.1f, 0.0f, 0.0f), Vector3.down, rotation);
+            }
+            if (whichdoor == 1)
+            {
+                transform.RotateAround(new Vector3(-4.9f, 0.0f, 0.0f), Vector3.up, rotation);
+            }
+        }
+
+        
+    }
+}
