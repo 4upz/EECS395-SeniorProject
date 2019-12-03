19bcfb38a22dab35aa962dd54ea55d0a5589daaa
Daniel <danielrubin17@gmail.com>
2019-12-02 22:58:01 -0500

    Script for having in game menu popup

--- SchizophreniaSimulator/Assets/Scripts/UIControls/InGameMenu.cs
+++ SchizophreniaSimulator/Assets/Scripts/UIControls/InGameMenu.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Valve.VR;
 
 public class InGameMenu : MonoBehaviour
 {
+    private bool previousState = false;
+    [SerializeField] private GameObject menuScreen;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,14 @@ public class InGameMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        bool state = SteamVR_Actions._default.GrabGrip[SteamVR_Input_Sources.RightHand].state;
+        if (state && !previousState)
+        {
+            if (menuScreen.activeInHierarchy)
+                menuScreen.SetActive(false);
+            else
+                menuScreen.SetActive(true);
+        }
+        previousState = state;
     }
 }
