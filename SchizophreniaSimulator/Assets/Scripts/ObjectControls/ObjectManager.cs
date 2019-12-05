7e2bd3ffc99a2cdc0da17f2806c946bf77dafe0d
Arik Stewart <ams382@case.edu>
2019-12-05 03:58:26 -0500

    Modified object transformation and default locations of toys

--- SchizophreniaSimulator/Assets/Scripts/ObjectControls/ObjectManager.cs
+++ SchizophreniaSimulator/Assets/Scripts/ObjectControls/ObjectManager.cs
@@ -7,28 +7,34 @@ public class ObjectManager : MonoBehaviour
 {
     private GameObject targetObject; // The hazard tyo be modified
     public Transform[] spawnPoints; // Array of spawn transformations to be moved to
+    public PuzzleManager puzzleManager;    // The current puzzle manager of this scene
+    public float transformDelay;            // The delayed time before this object is moved
 
-    private bool objectMovementTriggered; // Boolean trigger for randomized object movement
+    private bool objectMovementTriggered;   // Boolean trigger for randomized object movement
+    private bool hasBeenTranslated;         // Whether this object has been moved
 
     //Upon Start of Scene
     void Start(){
         // Variable just for clarification of referenced object
         targetObject = gameObject;
-
-        /* This is just for testing the script. Remove when done */
-        IEnumerator trigger = SetTriggerAfterTime(10.0f);
-        StartCoroutine(trigger);
+        hasBeenTranslated = false;
     }
 
     //Upon every frame
     void Update(){
-        if (objectMovementTriggered){
+        if (objectMovementTriggered && !hasBeenTranslated){
             MoveObjectToRandomLocation();
+        }
+        
+        if (puzzleManager.puzzles[puzzleManager.activePuzzleIndex].goalObject == gameObject && !hasBeenTranslated){
+            IEnumerator trigger = SetTriggerAfterTime(3.0f);                                   // This will trigger the Object transformation
+            StartCoroutine(trigger);
         }
     }
 
    // Move the item to a new random location (probably a terrible name)
    private void MoveObjectToRandomLocation(){
+        hasBeenTranslated = true;
        //Find a random index between zero and one less than the number of spawn points
        int spawnPointIndex = Random.Range(0, spawnPoints.Length);
        Transform newTransform = spawnPoints[spawnPointIndex];
@@ -40,7 +46,7 @@ public class ObjectManager : MonoBehaviour
 
     // Alternative function that allows the object movement to be triggered after a certain amount of seconds
    public IEnumerator SetTriggerAfterTime(float waitTime){
-       yield return new WaitForSeconds(waitTime);
+        yield return new WaitForSeconds(waitTime);
        objectMovementTriggered = true;
    }
 
